c5e53a14cc106380a1e6490a37a0576c33bcd86d
Trulying <maciek.widera@gmail.com>
2016-07-25 00:22:09 +0200

    deleted Address class, added ContactDetails and Person class

--- src/main/java/com/widera/entities/Address.java
+++
@@ -1,7 +0,0 @@
-package com.widera.entities;
-
-/**
- * Created by maciek on 24.07.16.
- */
-public class Adress {
-}
---
+++ src/main/java/com/widera/entities/Person.java
@@ -0,0 +1,58 @@
+package com.widera.entities;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by maciek on 24.07.16.
+ */
+
+@MappedSuperclass
+public abstract class Person extends BaseEntity {
+
+    @NotEmpty
+    @Column(name = "name")
+    private String name;
+    @NotEmpty
+    @Column(name = "surname")
+    private String surname;
+    @NotNull
+    @Embedded
+    private ContactDetails contactDetails;
+
+    public Person(Long id, String name, String surname, ContactDetails contactDetails) {
+        super(id);
+        this.name = name;
+        this.surname = surname;
+        this.contactDetails = contactDetails;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public ContactDetails getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(ContactDetails contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+}
